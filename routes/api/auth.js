e1d535f3b2156c2d0e3546f5b8863461c5e6f618
Lenovo-PC\samwil <sgost@asu.edu>
2019-05-21 17:24:48 -0700

    verify the token done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+//middleware
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //Public GET route
-router.get('/', (req, res) => res.send('Auth route'));
+//this is middleware.
+//               ||
+//               \/
+router.get('/', auth, (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
